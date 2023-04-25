212330df7adbd8a3e09c7aff7555898f7f7edf2e
miillaadddddd <milad7212@gmail.com>
2023-04-25 08:22:22 +0330

    first commit

---
+++ src/views/LearnView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="h-screen bg-red-400">
+    <h1 class="font-bold text-2xl">{{}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { a: 1 }
+  },
+  methods: {
+    changeNumber(value) {
+      return ` ${value} Toman`
+    }
+  },
+  filters:{
+
+  }
+}
+</script>
